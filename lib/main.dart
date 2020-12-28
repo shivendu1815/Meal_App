2e41c33720c733aee36231fc01b19650ec840ec4
shivendu1815 <shivendu.sharma_cs18@gla.ac.in>
2020-12-28 19:15:22 +0530

    categories add on home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,33 +13,49 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 224, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          body2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotCondensed',
+            fontWeight: FontWeight.bold,
+          )
+        ),
       
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
+// class _MyHomePageState extends State<MyHomePage> {
  
-  @override
-  Widget build(BuildContext context) {
+//   @override
+//   Widget build(BuildContext context) {
     
-    return Scaffold(
-      appBar: AppBar(
+//     return Scaffold(
+//       appBar: AppBar(
  
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-       child: Text('Navigation Time!'),
-      ),
-    );
-  }
-}
+//         title: Text('DeliMeals'),
+//       ),
+//       body: Center(
+//        child: Text('Navigation Time!'),
+//       ),
+//     );
+//   }
+// }
