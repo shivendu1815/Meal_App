ae2f38d241d22bd071d1fa4ddf4ef6ec4d2c1bea
shivendu1815 <shivendu.sharma_cs18@gla.ac.in>
2020-12-30 20:01:07 +0530

    recipies added

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,6 @@
+import 'package:Meal_App/models/category.dart';
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -13,15 +15,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String,String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold (
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The Recipies For The Category!',
-        ),
-      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+
+      }, itemCount: categoryMeals.length,),
     );
   }
 }
