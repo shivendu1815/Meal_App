924c06fad38b325fe1fff9963dd986b363b2f379
shivendu1815 <shivendu.sharma_cs18@gla.ac.in>
2020-12-31 13:34:54 +0530

    images added to the recipies

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:Meal_App/models/category.dart';
+import '../widgets/meal_item.dart';
 import 'package:flutter/material.dart';
-import './dummy_data.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -24,7 +25,12 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(title: categoryMeals[index].title, 
+          imageUrl: categoryMeals[index].imageUrl, 
+          duration: categoryMeals[index].duration, 
+          complexity: categoryMeals[index].complexity, 
+          affordability: categoryMeals[index].affordability,
+          );
 
       }, itemCount: categoryMeals.length,),
     );
