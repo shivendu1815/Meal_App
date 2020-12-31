ac045b2be0a44a768f702157fda336fe9b61ace7
shivendu1815 <shivendu.sharma_cs18@gla.ac.in>
2020-12-31 20:31:34 +0530

    ingredients added on the third page

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -9,7 +9,8 @@ class MealDetailScreen extends StatelessWidget {
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('$mealId'),),
+        title: Text('${selectedMeal.title}'),
+      ),
       // body: Center(
       //   child: Text('The Meal! - $mealId!'),
       // ),
@@ -23,6 +24,36 @@ class MealDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical:10),
+          child: Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          height: 170,
+          width: 300,
+          child: ListView.builder(
+            itemBuilder: (ctx, index) => Card(
+              color: Theme.of(context).accentColor,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 10,
+                ),
+                child: Text(selectedMeal.ingredients[index])),
+            ),
+            itemCount: selectedMeal.ingredients.length,
+          ),
+        )
       ],
     ),
   );
